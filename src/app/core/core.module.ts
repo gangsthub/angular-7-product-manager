bc7761d8eb1163086b825e12cac9d4e996c6e2eb
paul_melero <paulmelero@gmail>
2019-04-06 10:30:15 +0200

    [master] added sidebar and custom classes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@app/modules/material/material.module';
 
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, MaterialModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, SidebarComponent],
+  exports: [HeaderComponent, SidebarComponent],
   providers: []
 })
 export class CoreModule {
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -3,9 +3,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatCardModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatCardModule, MatIconModule]
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule
+  ]
 })
 export class MaterialModule {}
